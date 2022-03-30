4cf52d88e92dbdc8ab54a3f0dc6e6a995df3b7db
Guilherme Roos <guilherme.roosr@gmail.com>
2022-03-29 21:43:53 -0300

    created quit and resume commands

---
+++ slash/quit.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("quit").setDescription("Stops the bot and clears the queue"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem músicas na fila...")
+
+		queue.destroy()
+    await interaction.editReply("Falo rapeize :call_me:")
+	},
+}
---
+++ slash/resume.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("resume").setDescription("Resumes the music"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem músicas na fila")
+
+		queue.setPaused(false)
+        await interaction.editReply("Música despausada! Use '/pause' pausar a música")
+	},
+}
