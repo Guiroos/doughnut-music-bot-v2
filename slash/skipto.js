f82f0663e42696a986b3a2f3abd04c95b5e47259
Guilherme Roos <guilherme.roosr@gmail.com>
2022-03-29 21:44:20 -0300

    created shuffle, skip and skipto commands

---
+++ slash/skipto.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("skipto").setDescription("Skips to a certain track #")
+    .addNumberOption((option) => 
+        option.setName("tracknumber").setDescription("The track to skip to").setMinValue(1).setRequired(true)),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem músicas na fila...")
+
+        const trackNum = interaction.options.getNumber("tracknumber")
+        if (trackNum > queue.tracks.length)
+            return await interaction.editReply("Número da música inválido")
+		queue.skipTo(trackNum - 1)
+
+        await interaction.editReply(`Pulou para a música número ${trackNum}`)
+	},
+}
